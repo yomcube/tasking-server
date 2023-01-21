1b5942a703a0c9e814d3227b9b264b1092adc69c
TomCube <121310847+yomcube@users.noreply.github.com>
2023-01-21 17:45:13 -0600

    add logging

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 8000;
 const express = require('express');
 const app = express();
 
+app.use('/', (req, res) => {
+    console.log(req.method + " '" + req.originalUrl + "'");
+});
+
 app.get('/', (req, res) => {
 	res.set('Content-Type', 'text/plain');
     res.end('test');
