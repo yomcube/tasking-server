c490b89f8111634bd4aa150382ff551db085319a
TomCube <121310847+yomcube@users.noreply.github.com>
2023-01-22 15:09:41 -0600

    add signup page

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const port = 8000;
-
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
@@ -12,5 +12,9 @@ app.use('/', (req, res, next) => {
 app.get('/', (req, res) => {
 	res.set('Content-Type', 'text/plain');
     res.end('test');
+});
+app.post('/signup', (req, res) => {
+    // TODO: Add code here
+    res.end();
 });
 app.listen(8000);
