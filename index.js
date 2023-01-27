13e04bc7854f68c9c845a8ae8e18afa17f720241
TomCube <121310847+yomcube@users.noreply.github.com>
2023-01-26 19:38:54 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
 #!/usr/bin/env node
 const port = 8000;
+const accountdir = 'accounts/'
 const fs = require('fs');
+const cookieSession = require('cookie-session');
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use('/', (req, res, next) => {
-    console.log(req.method + " '" + req.originalUrl + "'");
-    next();
+	console.log(req.method + " '" + req.originalUrl + "'");
+	next();
 });
 
-app.get('/', (req, res) => {
+app.use('/', (req, res) => {
 	res.set('Content-Type', 'text/plain');
-    res.end('test');
+	res.end('test');
 });
-app.post('/signup', (req, res) => {
-    // TODO: Add code here
-    res.end();
+app.use('/signup', (req, res) => {
+	if (req.method != 'POST') {res.status(501).end(); return;}
+	if (!req.body.username || !req.body.password) {
+		res.set('Content-Type', 'text/plain');
+		res.status(400).end('must have both username and password parameters');
+		return;
+	}
+	fs.writeFile(accountdir + req.username + '/pass', req.password, (err) => {
+		if (err) {throw err; res.status(500).end('Something went wrong.'); return;}
+		else res.status(201).end('200 Created');
+	});
+});
+app.use('/login', (req, res) => {
+	if (req.method != 'POST') {res.status(501).end(); return;}
+	if (!req.body.username || !req.body.password) {res.set('Content-Type', 'text/plain');
+		res.status(400).end('must have both username and password parameters');
+		return;
+	}
+	fs.readFile(accountdir + req.username + '/pass', (err, data) => {
+		if (err) {throw err; res.status(500).end('Something went wrong.'); return;}
+		if (req.password.toString() == data) {
+			req.session.user = req.body.username;
+			res.status(200).end('200 OK');
+			return;
+		}
+		res.status(400).end();
+	});
 });
 app.listen(8000);
